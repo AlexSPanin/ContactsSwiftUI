79112dc50ff2d9f5b7d05e71a22bfa02f7679be2
Alexander Panin <panin1970@gmail.com>
2022-03-03 08:40:04 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Александр Панин on 03.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
